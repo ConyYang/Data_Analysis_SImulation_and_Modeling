dcb2aaa2c2d384e5f54ce69d0dec51b635a0ce11
ConyYang <yangyubei0218@gmail.com>
2021-04-05 10:44:37 +0800

    random number generate .csv files

--- RandomNumberGenerate/generateRandomNumber.py
+++ RandomNumberGenerate/generateRandomNumber.py
@@ -1,11 +1,10 @@
-
 import numpy.random as random
 import pandas as pd
 import numpy as np
 
 
 def baseStation():
-    return int(random.uniform(1,21))
+    return int(random.uniform(1, 21))
 
 
 def interArrivalTime():
@@ -13,7 +12,7 @@ def interArrivalTime():
 
 
 def callDuration():
-    return 10+random.exponential(109.83590)
+    return 10 + random.exponential(109.83590)
 
 
 def carSpeed():
@@ -21,22 +20,25 @@ def carSpeed():
 
 
 def direction():
-    if random.uniform()<0.5:
+    if random.uniform() < 0.5:
         return 1
     else:
         return 0
 
 
 def position():
-    return random.uniform()*2000
+    return random.uniform() * 2000
 
 
 if __name__ == '__main__':
-    df = pd.DataFrame(columns=['BaseStation', 'interArrivalTime', 'callDuration', 'carSpeed', 'direction', 'position'])
-    for i in range(1, 101):
-        arr = [int(baseStation()), interArrivalTime(), callDuration(), carSpeed(), int(direction()), position()]
-        # baseStation(), interArrivalTime(), callDuration(), carSpeed(), direction(), position()]
-        df.loc[i] = arr
-    df.to_csv('test.csv')
-
-
+    for i in range(2, 51):
+        df = pd.DataFrame(columns=['BaseStation', 'ArrivalTime', 'callDuration', 'carSpeed', 'direction', 'position', 'interArrivalTime'])
+        absolute_arrival_time = 0
+        for j in range(1, 10001):
+            iat = interArrivalTime()
+            absolute_arrival_time += iat
+            arr = [int(baseStation()), absolute_arrival_time, callDuration(), carSpeed(), int(direction()), position(),iat]
+            # baseStation(), interArrivalTime(), callDuration(), carSpeed(), direction(), position()]
+            df.loc[j] = arr
+        df.to_csv('SimulationData/data'+str(i)+'.csv')
+        print(str(i) + " done")
